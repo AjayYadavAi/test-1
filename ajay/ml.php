f86372ef1f80e97ab706521f99170a0dc9ab27b1
AjayYadavGBI <ajay_yadav@gbinternational.in>
2019-09-28 12:57:51 +0530

    Added ml.php

---
+++ ajay/ml.php
@@ -0,0 +1,109 @@
+.....
+supervised learing...
+	machine learns under guided, learns of the feeding data with output in data. input and output is labeled....
+
+
+
+unsupervised learing....
+like we learn on ourself without guidance.
+
+reinforcement learing....
+agent learn on taking action and experience
+learn on the bases of experience.
+hit and trail method used to learn.
+
+----------------------------------------
+
+Problem type....
+	1. Supervised
+		1. Regression - continuous quintity (persion weight) don't need to label data into different class.
+		2 classification (spam or not)
+
+2. unsupervised -   
+   1. Association ( relationship between bread and jamm , patterns in data)
+	people who tend to by bread , and people who want to by jamm
+   2. clustering ( buyers on the basis of their interest)  
+       cluster the customer on the basis of their semilirities.
+
+3. Reinforcement learning.....
+  
+
+-----------------------------------------------
+
+Types of data.....
+  1. labelled data in supervised data
+  2. unlabelled data ( cluster on the basis of their semilarities)
+  3. No predefined data ... 
+
+--------------------------------------------------
+
+Aim..
+1. supervised - forcast outcomes
+2. unsupervised - Discover underlying patterns.
+3. Learn series of action
+
+---------------------------------------------------
+Approach....
+1.supervised - Map labelled input to known output
+2. Understand patterns and discover output
+3. Follow trail and Error Method
+
+---------------------------------------------------
+
+Feedback...
+1. supervised - Training
+2. unsupervised - Explore patterns & trends
+3. Reinforcement - Reward
+
+--------------------------------------------------
+Popular Algorithms....
+1. supervised 
+    Linear Regression
+    Logistic Regression
+    Support Vector Machine
+    K nearest Neighbour
+    Random Forest
+2. Unserperviesed 
+    k-Means
+    Apriori
+    C-Means
+3. Reinforcement Learning
+    Q-learning 
+    SARSA
+------------------------------------------------------
+Applications
+1. super...
+	1.Risk Evaluation
+	2. Forecast Sales
+2. Unser.....
+	1.Recommendation Systems
+	2. Anomaly Detection (credit card fraud detection)
+3. Reinforcement Learning
+	1. Self Driving car
+	2. Gaming
+--------------------------------------------------------
+
+use cases...
+1. Study a bank credit dataset and make a decision about wheather to approve the load and applicaiton based on his profile.
+   use.. KNN algorithm ( approve or not )
+2. Mathematical equation for distance as a functuion fo speed so you can it to predict distance when only the speed of the ar is known 
+ use . Linear Regression algorithm( predict the distance, when the speed of a car is given) continuous case
+
+3.  to cluster a set of movies as either good or average based on their social media out reach
+   k-Means algorithms ( popular movies ) (Not popular movie)
+
+4.Peroform market basket analysis by finding association between items bought at a the grocery store
+	use ( Association rule mining & apriori algorithem ) 
+	perform market basket analysis
+
+5. Place an agent in any on of the rooms(0,1,2,3,4,5) and the goal is to reach outside the building (room5)
+	. Q-learing Algorithm ) Reinformation learing
+
+
+
+
+
+
+
+
+
